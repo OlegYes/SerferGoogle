8965f35d5d7391bc76a644d2d4e15f483dfd17bf
OlegYesikov <o.yesikov@gmail.com>
2022-05-06 16:34:05 +0200

    почав роботу з парсером 2

--- parser.py
+++ parser.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup as BS
 import re
 URL = "https://www.google.com/search?q=chees"
 def start_ref(q):
+    global data
+    data = {}
     headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"
@@ -16,11 +18,19 @@ def start_ref(q):
     soup = BS(src, "lxml")
 #    menu = soup.find("h3").find_parents("div", id="search").
     menu = soup.find_all("h3")
-    for i in menu:
-        inmenu = i.find_parent().find_parent().find_parent().find_parent()
 
-        print(inmenu)
-        lincs = i.get("href")
+
+    for i in menu:
+
+        inmenu = i.find_parent().find_parent().find_parent().find_parent()
+        for q in 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:
+            lincs = inmenu.get("href")
+            h3 = inmenu.find("h3").text
+            span = inmenu.find("span")
+
+            data = {q: [lincs, h3, span]}
+            print(data)
+
 
     return 0
 
